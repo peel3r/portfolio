d0e00ec544af8fb96d473a63f9c24edbb53d451e
pjakubowski <p@weljakubowski.com>
2016-08-14 21:33:41 +0100

    adds bootstrap

---
+++ src/components/Youtube/videoDetail.js
@@ -0,0 +1,16 @@
+import React, {PropTypes} from 'react';
+
+clas
+videoDetail = (props) => {
+    render()
+    {
+
+    }
+    ;
+}
+
+videoDetail.propTypes = {
+    myProp: PropTypes.string.isRequired
+};
+
+export default videoDetail;  
